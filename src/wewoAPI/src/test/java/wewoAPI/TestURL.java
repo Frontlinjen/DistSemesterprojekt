65be371603a76a4b0d4f589021a689d2664f5b0e
Hjorthen <Hjorthensebastian@gmail.com>
2017-03-12 09:27:32 +0100

    Added URL class

---
+++ src/wewoAPI/src/test/java/wewoAPI/TestURL.java
@@ -0,0 +1,10 @@
+package wewoAPI;
+import javabogLogin.URL;
+public class TestURL {
+
+	public static void main(String[] args) {
+		URL url = new URL("https://eu-west-1.console.aws.amazon.com/console/home?region=eu-west-1");
+		System.out.println(url);
+	}
+	
+}
