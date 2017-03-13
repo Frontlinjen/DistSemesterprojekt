241fb0231ed3ba4c11ee30644266fd1e180ee54f
MikkelLeth <mkleth10@gmail.com>
2017-03-13 09:50:09 +0100

    Updated Connection info

--- src/wewoAPI/src/main/java/DatabaseController/Constant.java
+++ src/wewoAPI/src/main/java/DatabaseController/Constant.java
@@ -3,10 +3,10 @@ package DatabaseController;
 public abstract class Constant
 {
 	public static final String
-		server					= "gruppe12.dynu.com",  // database-serveren
-		database				=  "CDIOFINAL",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "gruppe12", // dit brugernavn = dit studienummer 
-		password				= "je2&j=Sd"; // dit password som du har valgt til din database
+		server					= "wewodb.clintemoyfkc.eu-west-1.rds.amazonaws.com",  // database-serveren
+		database				=  "WewoDB",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "root", // dit brugernavn = dit studienummer 
+		password				= "FRONTLINJEN88"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
