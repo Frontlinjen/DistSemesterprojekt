24becf7d629c557a418b80ac9ed0ec47ca45e91e
Senad Begovic <senadbegovic2500@hotmail.com>
2017-04-03 08:18:49 +0200

    Fixed some logic in MySQLRating

--- src/wewoAPI/src/main/java/DatabaseController/MySQLRatingRepository.java
+++ src/wewoAPI/src/main/java/DatabaseController/MySQLRatingRepository.java
@@ -1,10 +1,21 @@
 package DatabaseController;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQLRatingRepository implements RatingRepository{
 
 	private final String GET_RATING = "SELECT * FROM UserRatings WHERE raterID = ? AND rateeID = ?;";
 	private final String CREATE_RATING = "INSERT INTO UserRatings(rating, raterID, rateeID, message) "
 									   + "VALUES (?, ?, ?, ?);";
 
+
+	public MySQLRatingRepository(){
+		DatabaseConnector.RegisterStatement("GET_TASK", GET_RATING);
+		DatabaseConnector.RegisterStatement("CREATE_TASK", CREATE_RATING);
+	}
+	
 	public RatingDTO getRating(RatingDTO rate) throws DALException {
 		// TODO Auto-generated method stub
 		return null;
