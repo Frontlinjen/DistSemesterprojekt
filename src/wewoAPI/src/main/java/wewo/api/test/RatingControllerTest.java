84d7f89428b7b8174427c9720c1cd6575c9fab90
Senad Begovic <senadbegovic2500@hotmail.com>
2017-04-03 09:43:04 +0200

    Added a test class for rating, so far without any logic in it

---
+++ src/wewoAPI/src/main/java/wewo/api/test/RatingControllerTest.java
@@ -0,0 +1,12 @@
+package wewo.api.test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import wewoAPI.RatingController;
+import modelPOJO.*;
+
+public class RatingControllerTest {
+
+	public static void main (String[]args){
+		
+	}
+}
