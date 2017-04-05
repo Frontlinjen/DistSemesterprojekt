788e9ebd03da42961159cdb693ebe37f375e87b4
MikkelLeth <mkleth10@gmail.com>
2017-04-05 14:36:48 +0200

    Added classes to make database control for the table Account

--- src/wewoAPI/src/main/java/wewo/api/test/AccountControllerTest.java
+++ src/wewoAPI/src/main/java/wewo/api/test/AccountControllerTest.java
@@ -1,5 +1,46 @@
 package wewo.api.test;
 
-public class AccountControllerTest {
+import java.util.ArrayList;
+import java.util.List;
 
+import modelPOJO.Account;
+import modelPOJO.FindDataObject;
+import modelPOJO.IDObject;
+import modelPOJO.Task;
+import wewoAPI.AccountController;
+import wewoAPI.TaskController;
+
+public class AccountControllerTest {
+	public static void main (String[]args){
+		List<String> competences = new ArrayList<String>();
+		competences.add("Børnepasning");
+		competences.add("Slå græs");
+		
+		AccountController AC = new AccountController();
+		Account account = new Account();
+		//task.setID("ID");
+		account.setName("Test Et");
+		account.setEmail("test@test.dk");
+		account.setPhone("12345678");
+		account.setSex(true);
+		account.setAboutMe("Hej");
+		account.setCompetences(competences);
+		IDObject ID = new IDObject();
+		ID.setID(7);
+		FindDataObject FD = new FindDataObject();
+		
+		
+		ContextTest cont = new ContextTest();
+		
+		
+		//System.out.println(AC.createAccount(account, cont).getID());
+		Account a = AC.getAccount(ID, cont);
+		
+		a.getCompetences();
+		//AC.findAccounts(FD, cont);
+		
+		//AC.updateAccount(task, cont);
+		
+		//AC.deleteAccount(ID, cont);
+	}
 }
