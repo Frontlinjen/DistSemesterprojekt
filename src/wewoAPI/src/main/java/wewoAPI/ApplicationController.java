fddd7bb7a443e7a8293f96f370a6a5a8fc91e59e
Nicki <nduris@hotmail.com>
2017-04-05 16:56:48 +0200

    Began work on ApplicationController

--- src/wewoAPI/src/main/java/wewoAPI/ApplicationController.java
+++ src/wewoAPI/src/main/java/wewoAPI/ApplicationController.java
@@ -1,14 +1,18 @@
 package wewoAPI;
 import com.amazonaws.services.lambda.runtime.Context;
 
+import exceptions.BadRequestException;
 import exceptions.UnauthorizedException;
 import modelPOJO.Application;
 import modelPOJO.IDObject;
 import modelPOJO.JsonList;
 import modelPOJO.ResponseObject;
+import DatabaseController.ApplicationDTO;
 import DatabaseController.ApplicationRepository;
 import DatabaseController.DALException;
 import DatabaseController.MySQLApplicationRepository;
+import DatabaseController.MySQLTaskRepository;
+import DatabaseController.TaskDTO;
 
 public class ApplicationController {
 	
@@ -24,23 +28,57 @@ public class ApplicationController {
 	{
 		this.repository = repository;
 	}
-		
-	public JsonList<String> GetApplicants(IDObject taskid, Context context)
-	{
-		return null;
-	}
 	
-	public void PostApplications(Application app, Context context) throws UnauthorizedException, NumberFormatException, DALException
+	private void verifyLogin(Context context) throws UnauthorizedException
 	{
-		
-		
 		if(context.getIdentity() == null || context.getIdentity().getIdentityId().isEmpty())
 		{
 			throw new UnauthorizedException("Invalid login");
+		}	
+	}
+		
+	public JsonList<String> GetApplicants(IDObject taskid, Context context) throws UnauthorizedException, DALException
+	{
+		MySQLTaskRepository tas = new MySQLTaskRepository();
+		
+		verifyLogin(context);
+		if(tas.getTask(taskid.getID()).getCreatorId() != context.getIdentity().getIdentityId()){
+			throw new UnauthorizedException("Du har ikke adgang til dette.");
 		}
+		try {
+			tas.getTaskList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void PostApplications(Application app, Context context) throws UnauthorizedException, NumberFormatException, DALException, BadRequestException
+	{
+		ApplicationDTO dto = ApplicationDTO.fromModel(app);
+//		IDObject newApplierID = new IDObject();
+		
+		verifyLogin(context);
+		
 		if(repository.getApplication(Integer.parseInt(app.getApplierid())) != null) {
-			
+			throw new BadRequestException("Du har allerede en application på denne task.");
 		}
+		
+		dto.setApplierid(context.getIdentity().getIdentityId());
+		
+		try {
+			Application application = new Application();
+			application.setTaskid(dto.getTaskid());
+			application.setApplierid(dto.getApplierid());
+			application.setApplicationMessage(dto.getApplicationMessage());
+			repository.createApplication(dto);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		return newApplierID;
+		
 		
 	}
 	
