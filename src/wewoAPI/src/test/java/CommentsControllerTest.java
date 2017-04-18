ffd6f0017d78b1f3f4328c7c15da469e1a835a7d
Thomas Bech Madsen <thomasbm@live.dk>
2017-04-18 11:45:03 +0200

    Work on test

--- src/wewoAPI/src/test/java/CommentsControllerTest.java
+++ src/wewoAPI/src/test/java/CommentsControllerTest.java
@@ -1,7 +1,10 @@
 import static org.junit.Assert.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,7 +31,6 @@ public class CommentsControllerTest {//Tests kan ikke se CommentsController?? Hæ
 		controller = new CommentsController(new MockCommentsRepository());
 		context = new ContextTest("TestUser");
 		controller.createComment(generateTestData(), context);
-		
 	}
 	
 	private Comment generateTestData()
@@ -68,5 +70,74 @@ public class CommentsControllerTest {//Tests kan ikke se CommentsController?? Hæ
 			e.printStackTrace();
 		}
 	}
-
+	
+	@Test
+	private void getComment(){
+		Comment comment = generateTestData();
+		Comment comment2 = generateTestData();
+		comment2.setID(22);
+		try {
+			IDObject id = controller.createComment(comment, context);	
+			controller.createComment(comment2, context);
+			DoubleIDObject dido = new DoubleIDObject();
+			dido.setFirstID(comment.getTaskID());
+			dido.setSecondID(comment.getID());
+			assertEquals(controller.getComment(dido, context), comment);
+			IDObject ido = new IDObject();
+			ido.setID(comment.getTaskID());
+			List<Comment> rList = new ArrayList<Comment>();
+			rList.add(comment);
+			rList.add(comment2);
+			assertEquals(((List<Comment>)controller.getCommentList(ido, context)),rList);
+		} catch (NotFoundException e) {
+			fail("Task was not created");
+		} catch (UnauthorizedException e) {
+			fail("User was not authorized");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	private void updateComment(){
+		Comment comment = generateTestData();
+		Comment comment2 = generateTestData();
+		comment2.setText("blah");
+		try {
+			IDObject id = controller.createComment(comment, context);
+			DoubleIDObject dido = new DoubleIDObject();
+			dido.setFirstID(comment.getTaskID());
+			dido.setSecondID(comment.getID());
+			Comment reComment1 = controller.getComment(dido, context);
+			controller.updateComment(comment2, context);
+			DoubleIDObject dido2 = new DoubleIDObject();
+			dido2.setFirstID(comment2.getTaskID());
+			dido2.setSecondID(comment2.getID());
+			Comment reComment2 = controller.getComment(dido2, context);
+			assertEquals(comment, reComment1);
+			assertEquals(comment2, reComment2);
+			assertNotEquals(reComment1, reComment2);
+			assertTrue(reComment2.getText().equals("blah"));
+		} catch (NotFoundException e) {
+			fail("Task was not created");
+		} catch (UnauthorizedException e) {
+			fail("User was not authorized");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	private void deleteComment(){
+		Comment comment = generateTestData();
+		try {
+			controller.createComment(comment, context);
+			controller.deleteComment(comment, context);
+			DoubleIDObject dido = new DoubleIDObject();
+			controller.getComment(dido, context);
+			fail("Task was found");
+		} catch (NotFoundException e) {
+		} catch (UnauthorizedException e) {
+			fail("User was not authorized");
+			e.printStackTrace();
+		}
+	}
 }
