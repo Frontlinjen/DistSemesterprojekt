8f3aa48f812644d6dcb82560e6ca8000c1fc1a3f
Hjorthen <Hjorthensebastian@gmail.com>
2017-04-20 23:51:32 +0200

    Updated MockCommentRepository to ignore taskID

--- src/wewoAPI/src/main/java/mockRepositories/MockCommentsRepository.java
+++ src/wewoAPI/src/main/java/mockRepositories/MockCommentsRepository.java
@@ -3,6 +3,7 @@ package mockRepositories;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import DatabaseController.CommentDTO;
 import DatabaseController.CommentRepository;
@@ -14,33 +15,28 @@ import exceptions.BadRequestException;
 public class MockCommentsRepository implements CommentRepository{
 
 //HashMap<primary_key, CommentDTO> database = new HashMap<primary_key, CommentDTO>();
-List<List<CommentDTO>> database = new ArrayList<List<CommentDTO>>();
+private Map<Integer, CommentDTO> database = new HashMap<Integer, CommentDTO>();
 	
 	public CommentDTO getComment(int taskId, int commentId) throws DALException, BadRequestException {
-		try{
-			return database.get(taskId).get(commentId);
-		}
-		catch(BadRequestException e){
-			e.printStackTrace();
-		}
+		return database.get(commentId);
 	}
 
 	public List<CommentDTO> getCommentList(int taskId) throws DALException, BadRequestException {
-		return database.get(taskId);
+		return new ArrayList<CommentDTO>(database.values());
 	}
 
 	public int createComment(CommentDTO com) throws DALException, BadRequestException {
-		database.get(com.getTaskID()).add(com.getID(),com); //Exception if task does not exist
+		database.put(com.getID(),com); //Exception if task does not exist
 		return database.size() - 1;
 	}
 
 	public int updateComment(CommentDTO com) throws DALException, BadRequestException {
-		database.get(com.getTaskID()).add(com.getID(),com); //Exception if task does not exist
+		database.put(com.getID(),com); //Exception if task does not exist
 		return 1;
 	}
 
 	public int deleteComment(int taskId, int commentId) throws DALException, BadRequestException {
-		database.get(taskId).remove(commentId);
+		database.remove(commentId);
 		return 1;
 	}
 }
