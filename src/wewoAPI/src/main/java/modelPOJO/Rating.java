0b4a54404e0940e1ae36880207318c6b30baee31
Senad Begovic <senadbegovic2500@hotmail.com>
2017-04-26 14:58:37 +0200

    Removed all getters and setters, and removed most of the variables, to make rating functions anonymous data

--- src/wewoAPI/src/main/java/modelPOJO/Rating.java
+++ src/wewoAPI/src/main/java/modelPOJO/Rating.java
@@ -2,40 +2,7 @@ package modelPOJO;
 
 public class Rating {
 
-	int rating, ratingID;
-	String raterID, rateeID, message;
-	
-	public String getRaterID() {
-		return raterID;
-	}
-	public void setRaterID(String raterID) {
-		this.raterID = raterID;
-	}
-	public int getRatingID() {
-		return ratingID;
-	}
-	public void setRatingID(int ratingID) {
-		this.ratingID = ratingID;
-	}
-	
-	public String getRateeID() {
-		return rateeID;
-	}
-	public void setRateeID(String rateeID) {
-		this.rateeID = rateeID;
-	}
-	
-	public int getRating() {
-		return rating;
-	}
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
+	public float rating;
+	public String message; 
 	
 }
